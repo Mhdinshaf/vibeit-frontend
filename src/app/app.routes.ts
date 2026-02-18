fb33a347b049575aec313626c69cc26553750dee
Mohomed Inshaf <164790727+Mhdinshaf@users.noreply.github.com>
2026-02-19 00:16:27 +0530

    feat: Add core components for cart, contact, footer, home, login, nav, products, and shop pages
    - Implemented Cart component with basic structure and test cases.
    - Created Contact component with a form and information layout.
    - Developed Footer component for site-wide footer content.
    - Established Home component featuring promotional content and product categories.
    - Added Login component for user authentication.
    - Built Nav component for site navigation.
    - Created Products component to display product details and reviews.
    - Developed Shop component with filters and product listings.
    - Included CSS and HTML files for all components to enhance styling and layout.
    - Added unit tests for each component to ensure functionality.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { Home } from './page/home/home';
+import { Shop } from './page/shop/shop';
+import { Products } from './page/products/products';
+import { Cart } from './page/cart/cart';
+import { Login } from './page/login/login';
+import { Contact } from './page/contact/contact';
+import { About } from './page/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:Home
+    },
+    {
+        path:'shop',
+        component:Shop
+    },
+    {
+        path:'product',
+        component:Products
+    },
+    {
+        path:'cart',
+        component:Cart
+
+    },
+    {
+        path:'login',
+        component:Login
+    },
+    {
+        path:'about',
+        component:About
+    },
+    {
+        path:'contact',
+        component:Contact
+    }
+
+];
