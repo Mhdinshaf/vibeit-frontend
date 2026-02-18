0a9c72d3f3535740ea03fe770e66fb3c68d7d406
Mohomed Inshaf <164790727+Mhdinshaf@users.noreply.github.com>
2026-02-18 23:43:41 +0530

    feat: initialize Angular application with basic structure and styles
    - Added main application files including app component, routes, and styles.
    - Created app.html and app.css for the main layout and styling.
    - Implemented routing with app.routes.ts.
    - Set up testing framework with app.spec.ts.
    - Configured TypeScript settings with tsconfig files.
    - Included global styles and imported necessary libraries in styles.css.
    - Added index.html as the entry point for the application.

---
+++ src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  title = 'vibeit';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
